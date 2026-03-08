77a7ab6d28dd610210eb695cb2ab8ad27b464e0e
Hai_Tay_Wakai <sslikoolik197@gmail.com>
2026-03-09 05:42:43 +0700

    Update Worker.java

--- Lab5/src/employee/Worker.java
+++ Lab5/src/employee/Worker.java
@@ -4,7 +4,19 @@ public class Worker extends Employee {
     private double bonus;
 
     public void calBonus() {
-        bonus = getIncome() * 4; 
+        if (getID() > 100) {
+           
+            bonus = getIncome() * 4;
+        } else if (getID() >= 10) {
+           
+            bonus = getIncome() / 2.0;
+        } else if (getID() > 0) {
+           
+            bonus = getIncome() / 3.0;
+        } else {
+            
+            bonus = 0;
+        }
     }
 
     public double getBonus() {
